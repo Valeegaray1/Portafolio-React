04691a7d87733fefc3186df4727afdb2691400d6
Valeegaray1 <valeegaray1@gmail.com>
2022-07-28 09:45:37 -0300

    testimonials

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -14,7 +14,7 @@ export const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')}className={activeNav === '#' ? 'active' : ''}> <AiOutlineHome /> </a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}> <AiOutlineUser /> </a>
       <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <BiBook /> </a>
-      <a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}> <RiServiceLine /> </a>
+      <a href='#testimonials' onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}> <RiServiceLine /> </a>
       <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}> <BiMessageDetail /> </a>
     </nav>
   )
