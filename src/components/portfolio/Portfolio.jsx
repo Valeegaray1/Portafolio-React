1c7420125c7ac38c143bfb0922654472d8b37932
Valeegaray1 <valeegaray1@gmail.com>
2022-08-01 14:21:12 -0300

    nuevo proyecto

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
 
 export const Portfolio = () => {
   return (
@@ -13,11 +14,12 @@ export const Portfolio = () => {
       <div className="portfolio__container">
         <article className='portfolio_item'>
           <div className="portfolio__item-image">
-              <img src={IMG1} alt="" className='portfolio__prueba' />
+              <img src={IMG4} alt="" className='portfolio__prueba' />
           </div>
-          <h3>Contador de clicks React</h3>
+          <h3>Buscador de imágenes React</h3>
           <div className='porfolio__item-cta'>
-          <a href='https://github.com/Valeegaray1/ContadorClicks' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href='https://github.com/Valeegaray1/buscadordeimagenesReact' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href='https://valeegaray1.github.io/buscadordeimagenesReact/' className='btn' target='_blank' rel="noopener noreferrer">Github Pages</a>
           </div>
         </article>
         <article className='portfolio_item'>
@@ -27,6 +29,7 @@ export const Portfolio = () => {
           <h3>Calculadora React</h3>
           <div className='porfolio__item-cta'>
           <a href='https://github.com/Valeegaray1/calculadoraReact' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href='https://valeegaray1.github.io/calculadoraReact/' className='btn' target='_blank' rel="noopener noreferrer">Github Pages</a>
           </div>
         </article>
         <article className='portfolio_item'>
@@ -36,6 +39,16 @@ export const Portfolio = () => {
           <h3>Web React</h3>
           <div className='porfolio__item-cta'>
           <a href='https://github.com/Valeegaray1/apptestimonios2' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+          <a href='https://valeegaray1.github.io/appTestimonios/' className='btn' target='_blank' rel="noopener noreferrer">Github Pages</a>
+          </div>
+        </article>
+        <article className='portfolio_item'>
+          <div className="portfolio__item-image">
+              <img src={IMG1} alt="" className='portfolio__prueba' />
+          </div>
+          <h3>Contador de clicks React</h3>
+          <div className='porfolio__item-cta'>
+          <a href='https://github.com/Valeegaray1/ContadorClicks' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
           </div>
         </article>
       </div>
